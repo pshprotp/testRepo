c5b01cd0aaef4c93f4f6585e0afc5f185d37e19d
Сергей Александрович <shprot@MacBook-Pro-Sergej.local>
2017-11-08 20:33:04 +0200

    addVC3

---
+++ lesson_65_1_Gitignore/ViewController3.h
@@ -0,0 +1,13 @@
+//
+//  ViewController3.h
+//  lesson_65_1_Gitignore
+//
+//  Created by Сергей Александрович on 08.11.2017.
+//  Copyright © 2017 Sergey Podobnyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController3 : UIViewController
+
+@end
