2e4febc37104cfbc8599897a13fded4bffed10f9
Сергей Александрович <>
2017-11-08 20:30:18 +0200

    addVC2

---
+++ lesson_65_1_Gitignore/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  lesson_65_1_Gitignore
+//
+//  Created by Сергей Александрович on 08.11.2017.
+//  Copyright © 2017 Sergey Podobnyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
