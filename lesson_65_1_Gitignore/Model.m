23752c1c58565c96bb375e6a493a3ab99910d5f4
Сергей Александрович <shprot@MacBook-Pro-Sergej.local>
2017-11-08 20:42:16 +0200

    commit bufix

--- lesson_65_1_Gitignore/Model.m
+++ lesson_65_1_Gitignore/Model.m
@@ -1,26 +1,13 @@
 //
-//  ViewController.m
+//  Model.m
 //  lesson_65_1_Gitignore
 //
 //  Created by Сергей Александрович on 08.11.2017.
 //  Copyright © 2017 Sergey Podobnyy. All rights reserved.
 //
 
-#import "ViewController.h"
 #import "Model.h"
 
-@interface ViewController ()
-
-@end
-
-@implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    // mdel
-}
-
-
+@implementation Model
 
 @end
